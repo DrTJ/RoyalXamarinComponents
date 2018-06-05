842d96b98497510b0f0d8d9fde832d201ff63a1e
DrTJ <DrTJ2066@gmail.com>
2018-06-05 00:42:58 -0500

    Some codes organized.

---
+++ RoyalXamarinComponents/Controls/ListView.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace RoyalXamarinComponents.Controls
+{
+    public class ListView : Xamarin.Forms.ListView
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructors
+        public ListView()
+        {
+            ItemTapped += (sender, e) => 
+            {
+                if (ItemTappedCommand?.CanExecute(e.Item) ?? false)
+                {
+                    ItemTappedCommand?.Execute(e.Item);
+                }
+            };
+        }
+        #endregion
+
+        #region Properties
+
+        public static BindableProperty ItemTappedCommandProperty = BindableProperty.Create(
+            nameof(ItemTappedCommand),
+            typeof(Command<object>),
+            typeof(ListView),
+            propertyChanged: (bindable, oldValue, newValue) => {
+                var control = (ListView)bindable;
+                control.ItemTappedCommand = (ICommand)newValue;
+            });
+
+        public ICommand ItemTappedCommand
+        {
+            get { return (Command<object>)GetValue(ItemTappedCommandProperty); }
+            set { SetValue(ItemTappedCommandProperty, value); }
+        }
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
--- RoyalXamarinComponents/Layouts/RepeaterLayout.cs
+++ RoyalXamarinComponents/Layouts/RepeaterLayout.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace RoyalXamarinComponents
+namespace RoyalXamarinComponents.Layouts
 {
     public class RepeaterLayout: StackLayout
     {
