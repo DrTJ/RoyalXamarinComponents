57341d573b3fdec857e24a9e6ec42b449ce9566b
DrTJ <DrTJ2066@gmail.com>
2018-09-17 01:45:58 -0500

    ListView control updated to support Alternative data template.

--- RoyalXamarinComponents/Controls/ListView.cs
+++ RoyalXamarinComponents/Controls/ListView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,9 @@ namespace RoyalXamarinComponents.Controls
     public class ListView : Xamarin.Forms.ListView
     {
         #region Fields
+
+        private DataTemplate primaryTemplate;
+        private DataTemplate secondaryTemplate;
 
         #endregion
 
@@ -48,6 +52,63 @@ namespace RoyalXamarinComponents.Controls
             set { SetValue(ItemTappedCommandProperty, value); }
         }
 
+        public static BindableProperty UseAlternativeTemplateProperty = BindableProperty.Create(
+            nameof(UseAlternativeTemplate),
+            typeof(bool),
+            typeof(ListView), 
+            false);
+
+        public bool UseAlternativeTemplate
+        {
+            get { return (bool)GetValue(UseAlternativeTemplateProperty); }
+            set { SetValue(UseAlternativeTemplateProperty, value); }
+        }
+
+        public DataTemplate PrimaryTemplate
+        {
+            get => primaryTemplate;
+            set
+            {
+                if (Equals(primaryTemplate, value))
+                {
+                    return;
+                }
+
+                primaryTemplate = value;
+
+                // Generate Selector
+                if(primaryTemplate != null && secondaryTemplate != null)
+                {
+                    ResetTemplateSelectorSelector();
+                }
+            }
+        }
+
+        public DataTemplate SecondaryTemplate
+        {
+            get => secondaryTemplate;
+            set
+            {
+                if (Equals(secondaryTemplate, value))
+                {
+                    return;
+                }
+
+                secondaryTemplate = value;
+
+                // Generate Selector
+                if (primaryTemplate != null && secondaryTemplate != null)
+                {
+                    ResetTemplateSelectorSelector();
+                }
+            }
+        }
+
+        private void ResetTemplateSelectorSelector()
+        {
+            ItemTemplate = new AlternativeTemplateSelector(PrimaryTemplate, SecondaryTemplate);
+        }
+
         #endregion
 
         #region Commands
@@ -57,5 +118,37 @@ namespace RoyalXamarinComponents.Controls
         #region Methods
 
         #endregion
+    }
+
+    public class AlternativeTemplateSelector : DataTemplateSelector
+    {
+        private readonly DataTemplate primaryDataTemplate;
+        private readonly DataTemplate secondaryDataTemplate;
+
+        public AlternativeTemplateSelector(DataTemplate primaryDataTemplate, DataTemplate secondaryDataTemplate)
+        {
+            this.primaryDataTemplate = primaryDataTemplate;
+            this.secondaryDataTemplate = secondaryDataTemplate;
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var listView = container as ListView;
+            if (listView == null)
+                return null;
+            
+            double index = 0;
+            foreach (var itemInSource in listView.ItemsSource)
+            {
+                if(Equals(itemInSource, item))
+                {
+                    break;
+                }
+
+                index++;
+            }
+
+            return index % 2.0 == 0 ? primaryDataTemplate : secondaryDataTemplate;
+        }
     }
 }
